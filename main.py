6f4e60de8171850d86fbb81874c6f2de28a5a7b6
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-28 13:36:26 +0500

    json used

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import requests
+import requests, json
 
 url = 'http://api.open-notify.org/iss-now.json'
 response = requests.get(url)
 
 if response.status_code == 200:
-    print(response.text)
+    response_dictionary = json.loads(response.text)
+    position = response_dictionary['iss_position']
+    print('International Space Station at ' +
+        position['latitude'] + ', ' + position['longitude'])
 else:
     print("Houston, we have a problem:",response.status_code)    
