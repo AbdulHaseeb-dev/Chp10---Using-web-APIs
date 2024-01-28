bc035862470d25085428a722e414a1ca1b888962
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-28 14:04:06 +0500

    API completed

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import requests, json
+import requests, json, turtle
+
+def move_iss(lat, long):
+    global iss
+    iss.penup()
+    iss.goto(long, lat)
+    iss.pendown()
+
+screen = turtle.Screen()
+screen.setup(1000, 500)
+# screen.bgpic('earth.gif')
+screen.setworldcoordinates(-180, -90, 180, 90)
+
+iss = turtle.Turtle()
+iss.shape('circle')
+iss.color('red')
 
 url = 'http://api.open-notify.org/iss-now.json'
 response = requests.get(url)
@@ -8,5 +23,10 @@ if response.status_code == 200:
     position = response_dictionary['iss_position']
     print('International Space Station at ' +
         position['latitude'] + ', ' + position['longitude'])
+    lat = float(position['latitude'])
+    long = float(position['longitude'])
+    move_iss(lat, long)
 else:
     print("Houston, we have a problem:",response.status_code)    
+
+turtle.mainloop()    
